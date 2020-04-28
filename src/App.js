795bc189a5f3654562ce9e74770303845561b16e
Joao Otavio Chrisóstomo Barbosa <jobarbosa@sfiec.org.br>
2020-04-28 19:30:08 -0300

    feat: Add filter per country

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
 import React from 'react';
 
-import { Cards, CountryPicker, Charts } from './components';
-import { fetchData } from './api';
-
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/';
 import styles from './App.module.css';
 
+import image from './images/image.png';
 
 class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
 
-    state = {
-        data: {
+  async componentDidMount() {
+    const data = await fetchData();
 
-        }
-    }
+    this.setState({ data });
+  }
 
-    async componentDidMount() {
-        const fetchedData = await fetchData();
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
 
-        this.setState({ data: fetchedData })
-    }
+    this.setState({ data, country: country });
+  }
 
-    render() {
+  render() {
+    const { data, country } = this.state;
 
-        const { data } = this.state;
-        return (
-            <div className={styles.container}>
-                <Cards data={data} />
-                <CountryPicker />
-                <Charts />
-            </div>
-        );
-    }
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
+      </div>
+    );
+  }
 }
 
 export default App;
